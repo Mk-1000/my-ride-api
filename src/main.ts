7b6cea66eca6898f46ea17642193256e3ead0eab
Oussama Fekih Hassen <hssanoussama@gmail.com>
2024-10-06 20:18:51 +0200

    push

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,16 @@ import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { join } from 'path';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  app.enableCors({
+    origin:'http://localhost:3001',
+    methods:'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials:true,
+  })
   const config = new DocumentBuilder()
     .setTitle('My Ride API')
     .setDescription('API description')
@@ -12,6 +19,13 @@ async function bootstrap() {
     .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
+  
+  // Serve static files
+  app.useStaticAssets(join(__dirname, '..', 'uploads'), {
+    prefix: '/uploads/',
+  });
+
+
   app.useGlobalPipes(new ValidationPipe());
 
   await app.listen(3000);
